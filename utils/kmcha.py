ae105a747bef9a190c37ea6b7f1d20acab21c451
zwx <zwx>
2022-06-01 23:30:03 +0800

    kmcha

---
+++ utils/kmcha.py
@@ -0,0 +1,43 @@
+import urllib
+import urllib.parse
+import urllib.request
+import requests
+
+
+def kmcha_same(word):
+    page=requests.get('https://kmcha.com/similar/' + urllib.parse.quote(word))
+    page = page.text
+    # print(page)
+    similar = page.split("strong>%s的近义词"%(word))[-1]
+
+    similar = similar.split("<p>")[1].split("</p>")[0]
+    # print(similar)
+    ls = similar.split("<span>")
+    ret = []
+    for i, dt in enumerate(ls):
+        if i == 0:
+            continue
+        ret.append(dt.split("</span>")[0])
+    print(ret)
+    return ret
+
+def kmcha_similar(word):
+    page=requests.get('https://kmcha.com/similar/' + urllib.parse.quote(word))
+    page = page.text
+    # similar = page.split("strong>%s的近义词"%(word))[-1]
+    similar = page.split("strong>%s的相似词"%(word))[-1]
+
+    similar = similar.split("<p>")[1].split("</p>")[0]
+    # print(similar)
+    ls = similar.split("<span>")
+    ret = []
+    for i, dt in enumerate(ls):
+        if i == 0:
+            continue
+        ret.append(dt.split("</span>")[0])
+    print(ret)
+    return ret
+
+if __name__ == "__main__":
+    kmcha_same("卧龙")
+    kmcha_similar("卧龙")
